5da12cf9b343415f84c1635b4a1abe4f593bce10
gusman <gusman@DESKTOP-9VLAGMJ>
2017-12-20 20:26:17 +0100

    Initial commit

---
+++ OculusSpudControl/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace OculusSpudControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.eBtn = new System.Windows.Forms.Button();
+            this.dBtn = new System.Windows.Forms.Button();
+            this.sLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current status:";
+            // 
+            // eBtn
+            // 
+            this.eBtn.Enabled = false;
+            this.eBtn.Location = new System.Drawing.Point(15, 36);
+            this.eBtn.Name = "eBtn";
+            this.eBtn.Size = new System.Drawing.Size(75, 23);
+            this.eBtn.TabIndex = 1;
+            this.eBtn.Text = "Enable";
+            this.eBtn.UseVisualStyleBackColor = true;
+            this.eBtn.Click += new System.EventHandler(this.eBtn_Click);
+            // 
+            // dBtn
+            // 
+            this.dBtn.Enabled = false;
+            this.dBtn.Location = new System.Drawing.Point(96, 36);
+            this.dBtn.Name = "dBtn";
+            this.dBtn.Size = new System.Drawing.Size(75, 23);
+            this.dBtn.TabIndex = 2;
+            this.dBtn.Text = "Disable";
+            this.dBtn.UseVisualStyleBackColor = true;
+            this.dBtn.Click += new System.EventHandler(this.dBtn_Click);
+            // 
+            // sLbl
+            // 
+            this.sLbl.AutoSize = true;
+            this.sLbl.Location = new System.Drawing.Point(93, 9);
+            this.sLbl.Name = "sLbl";
+            this.sLbl.Size = new System.Drawing.Size(53, 13);
+            this.sLbl.TabIndex = 3;
+            this.sLbl.Text = "Unknown";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 71);
+            this.Controls.Add(this.sLbl);
+            this.Controls.Add(this.dBtn);
+            this.Controls.Add(this.eBtn);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Oculus SPUD control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button eBtn;
+        private System.Windows.Forms.Button dBtn;
+        private System.Windows.Forms.Label sLbl;
+    }
+}
+
---
+++ OculusSpudControl/Form1.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OculusSpudControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+
+                using (var rootKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+                {
+                    using (var sKey = rootKey.OpenSubKey("SOFTWARE"))
+                    {
+                        if (sKey == null)
+                        {
+                            MessageBox.Show("Cannot access Windows registry");
+                            Application.Exit();
+                            return;
+                        }
+
+                        using (var oKey = sKey.OpenSubKey("Oculus"))
+                        {
+
+                            if (oKey == null)
+                            {
+                                dBtn.Enabled = true;
+                                sLbl.Text = "Enabled";
+                                return;
+                            }
+
+                            var value = (int)oKey.GetValue("UseSpud", 1);
+
+                            if (value == 0)
+                            {
+                                eBtn.Enabled = true;
+                                sLbl.Text = "Disabled";
+                                return;
+                            }
+
+                            dBtn.Enabled = true;
+                            sLbl.Text = "Enabled";
+                            return;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Cannot access Windows registry");
+                Application.Exit();
+                return;
+            }
+        }
+
+        private void eBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                using (var rootKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+                {
+                    using (var sKey = rootKey.OpenSubKey("SOFTWARE", true))
+                    {
+                        if (sKey == null)
+                        {
+                            MessageBox.Show("Cannot access Windows registry");
+                            return;
+                        }
+
+                        sKey.DeleteSubKeyTree("Oculus");
+
+                        MessageBox.Show("Spud enabled, remember to restart the system to apply the configuration");
+                        eBtn.Enabled = false;
+                        dBtn.Enabled = true;
+                        sLbl.Text = "Enabled";
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Cannot access Windows registry");
+            }
+        }
+
+        private void dBtn_Click(object sender, EventArgs e)
+        {
+            using (var rootKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (var sKey = rootKey.OpenSubKey("SOFTWARE", true))
+                {
+                    if (sKey == null)
+                    {
+                        MessageBox.Show("Cannot access Windows registry");
+                        Application.Exit();
+                        return;
+                    }
+
+                    using (var oKey = sKey.CreateSubKey("Oculus", true))
+                    {
+                        oKey.SetValue("UseSpud", 0, RegistryValueKind.DWord);
+                        MessageBox.Show("Spud disabled, remember to restart the system to apply the configuration");
+                        eBtn.Enabled = true;
+                        dBtn.Enabled = false;
+                        sLbl.Text = "Disabled";
+                    }
+                }
+            }
+        }
+    }
+}
